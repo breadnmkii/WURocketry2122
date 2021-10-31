0d7f77f0d240251e852d7ccb3b992a7b8b52f149
Breadn <bylueang@gmail.com>
2021-10-31 13:15:31 -0500

    first gps2grid function drafted

--- Programs/position.py
+++ Programs/position.py
@@ -7,7 +7,8 @@
 #       3. Get difference (end - start)
 #       4. Convert to N/S, E/W distance measurements (meters)
 #       5. Assuming our launchpad @ (0,0), convert our distances to grid based on grid length (76.2 m)
-# Latitude: N (+), S (-)   # Longitude: E (+), W(-)
+# Latitude: N (+), S (-)    Longitude: E (+), W(-)
+# Latitude = y-axis         Longitude = x-axis
 import math
 from decimal import *
 
@@ -26,22 +27,28 @@ def coord2feet(coord):
     return ((Decimal(f'{coord[0]}')*Decimal(f'{LAT_DEGREE}')),
             (Decimal(f'{coord[1]}')*Decimal(f'{LON_DEGREE}')))
 
-# Since we know the image center will be at exactly half of 5000ft (2500,2500 ft), we can calculate distance from 
+# Since we know the image center will be at exactly half of 5000ft (2500,2500 ft), we can calculate distance from TOP-LEFT (0,0) 
+# by simply adding 2500ft to each axis of measurement. Then divide by grid length (250ft) and obtain ceiling to determine grid
+# we are in
 def coord2grid(launch_coord, current_coord):
     coord_correction = (Decimal(f'{launch_coord[0]}') - Decimal(f'{IDEAL_COORD[0]}'), 
                         Decimal(f'{launch_coord[1]}') - Decimal(f'{IDEAL_COORD[1]}'))
 
-    distance_from_launch = (Decimal(f'{current_coord[0]}') - Decimal(f'{launch_coord[0]}') + coord_correction[0],
-                            Decimal(f'{current_coord[1]}') - Decimal(f'{launch_coord[1]}') + coord_correction[1])
+    distance_from_launch = coord2feet((Decimal(f'{current_coord[0]}') - Decimal(f'{launch_coord[0]}') + coord_correction[0],
+                            Decimal(f'{current_coord[1]}') - Decimal(f'{launch_coord[1]}') + coord_correction[1]))
 
-    return coord2feet(distance_from_launch)
+    x_grid = math.ceil((distance_from_launch[1]+2500)/250)
+    y_grid = math.ceil((distance_from_launch[0]+2500)/250)
+
+    return (x_grid, y_grid)
+    
 
 
 
 
 def main():
     launch_coord = (38.663484, -90.365707)
-    current_coord = (38.664766, -90.363024)
+    current_coord = (38.663568, -90.366002)
     print(coord2grid(launch_coord, current_coord))
 
 if __name__=='__main__':
