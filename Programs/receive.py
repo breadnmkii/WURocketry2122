228256fe5000b63a8c2dcdb551dfc9a7e2c07992
Autumn Xu <autumn@wustl.edu>
2021-11-11 19:47:12 -0600

    Receiver work

---
+++ Programs/receive.py
@@ -0,0 +1,39 @@
+import time
+import busio
+import board
+from digitalio import DigitalInOut
+
+# Import the RFM9x radio module.
+import adafruit_rfm9x
+
+# Configure RFM9x LoRa Radio
+CS = DigitalInOut(board.CE1)
+RESET = DigitalInOut(board.D25)
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+
+file = open("receiving.txt", "a")
+
+while True:
+    # Attempt setting up RFM9x Module
+    try:
+        rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
+        rfm9x.tx_power = 23
+        print('RFM9x successfully set up!')
+        
+        while True:
+        
+            # RX
+            rx_packet = rfm9x.receive()
+            if rx_packet is None:
+                print('Did not receive')
+            else:
+                rx_data = str(rx_packet, "utf-8")
+                file.write(rx_data)
+                f = open("receiving.txt", "r")
+                print(f.read())
+                time.sleep(1)
+    
+    
+    except RuntimeError as error:
+        print('Error in setting up RFM9x... check wiring.')
+    
