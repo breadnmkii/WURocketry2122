65df735546fe7e278abb253ac4289b4bb665d037
Breadn <bylueang@gmail.com>
2021-11-14 11:52:09 -0600

    IMU mode testing

--- IMU_Test/imu_simpletest.py
+++ IMU_Test/imu_simpletest.py
@@ -9,7 +9,7 @@ import adafruit_bno055
 
 i2c = board.I2C()
 sensor = adafruit_bno055.BNO055_I2C(i2c)
-sensor.acceleration = 'IMUPLUS_MODE'
+sensor.mode = 'IMUPLUS_MODE'
 
 # If you are going to use UART uncomment these lines
 # uart = board.UART()
