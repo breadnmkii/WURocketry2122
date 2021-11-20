f0ced5c25182af5fb283009d9e3b51ae500fdbad
Breadn <bylueang@gmail.com>
2021-11-19 22:51:54 -0600

    Prototyping main

--- Programs/main.py
+++ Programs/main.py
@@ -138,6 +138,7 @@ def main():
     # Loop continuously gathers IMU data between hasLaunched and hasLanded
     while(not hasLanded):
         this_sample = time.monotonic()
+        print(this_sample - last_sample)
         if(this_sample - last_sample >= frequency):
             last_sample = this_sample
             lin_accel = imu.linear_acceleration
