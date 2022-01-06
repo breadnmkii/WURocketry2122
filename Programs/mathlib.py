353c784934a53c7aa5b11e443d60933aee8c08ec
Breadn <bylueang@gmail.com>
2022-01-06 14:18:51 -0600

    IMU Config work

--- Programs/mathlib.py
+++ Programs/mathlib.py
@@ -112,15 +112,15 @@ def quat_to_euler(w, x, y, z):
         """
         t0 = +2.0 * (w * x + y * z)
         t1 = +1.0 - 2.0 * (x * x + y * y)
-        roll_x = math.atan2(t0, t1)
+        roll_x = math.atan2(t0, t1)*(180/math.pi)
      
         t2 = +2.0 * (w * y - z * x)
         t2 = +1.0 if t2 > +1.0 else t2
         t2 = -1.0 if t2 < -1.0 else t2
-        pitch_y = math.asin(t2)
+        pitch_y = math.asin(t2)*(180/math.pi)
      
         t3 = +2.0 * (w * z + x * y)
         t4 = +1.0 - 2.0 * (y * y + z * z)
-        yaw_z = math.atan2(t3, t4)
+        yaw_z = math.atan2(t3, t4)*(180/math.pi)
      
-        return yaw_z, pitch_y, roll_x # in radians
+        return yaw_z, pitch_y, roll_x  # in degrees
