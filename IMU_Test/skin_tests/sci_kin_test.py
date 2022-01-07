b448bdfb0394765dc461a3273491a37a5c7c8bca
Breadn <bylueang@gmail.com>
2022-01-07 15:34:06 -0600

    IMU work

--- IMU_Test/skin_tests/sci_kin_test.py
+++ IMU_Test/skin_tests/sci_kin_test.py
@@ -73,16 +73,17 @@ if __name__ == '__main__':
 
     # Calibration step
     print("Calibrating BNO055...")
-    while(bno.calibration_status[1] != 3 and bno.calibration_status[2] != 3):
+    while(bno.calibration_status[1] != 3 or bno.calibration_status[2] != 3):
         print(bno.calibration_status)
     print("Calibrated!")
+    print(bno.calibration_status)
 
     # Initial orientation step (using quaternion)
     print("3 seconds to align to North...!")
     time.sleep(3)
     # init_orient = R.from_euler('zyx', [deg_N,90,0], degrees=false).as_matrix()   # Yaw, Pitch, Roll
     quat = bno.quaternion                        # [w,x,y,z]   scalar first format
-    init_orient = R.from_quat(quat[1:]+quat[0])  # [x,y,z]+[w] scalar last format
+    init_orient = R.from_quat(quat[1:]+tuple(quat[0]))  # [x,y,z]+[w] scalar last format
     print(init_orient)
 
     time.sleep(3)
