10733e921456b2f060da5051c42c3c8f42cefb09
Breadn <bylueang@gmail.com>
2022-01-11 20:00:29 -0600

    IMU position tracking + cumtrapz integration

--- IMU_Test/pos_track_test.py
+++ IMU_Test/pos_track_test.py
@@ -16,23 +16,22 @@ bno.accel_range = adafruit_bno055.ACCEL_8G
 
 def main():
     # Calibration step
-    print("Calibrating BNO055...")
-    while(bno.calibration_status[1] != 3 or bno.calibration_status[2] != 3):
-        pass
-    print("Calibrated!")
+    # print("Calibrating BNO055...")
+    # while(bno.calibration_status[1] != 3 or bno.calibration_status[2] != 3):
+    #     pass
+    # print("Calibrated!")
 
     while(input("Continue testing? (Y/n):").lower() == "y"):
         print("Collecting samples...")
 
         vals_acc = np.array([0,0,0])[np.newaxis]
 
-        samples = SAMPLES
+        count = 0
         last_sample = time.monotonic()
-        v0 = (0,0,0)
-        x0 = (0,0,0)
-        times = [0]          # Time steps
+        times = []          # Time steps
 
-        while samples > 0:
+        while count < SAMPLES:
+            count += 1
             this_sample = time.monotonic()
 
             if(this_sample-last_sample >= (1/SAMPLE_RATE)):
@@ -40,11 +39,10 @@ def main():
 
                 acc = bno.linear_acceleration   # m/s^2
                 quat = bno.quaternion
-                times.append(1/SAMPLE_RATE)
 
                 # Guard against Nonetype reads
                 if(acc[0] is not None and quat[0] is not None):
-                    samples -= 1
+                    times.append(count/SAMPLE_RATE)
                     
                     # Calculate rotation matrix
                     r_mat = R.from_quat((*(quat[1:]), quat[0])).as_matrix()
